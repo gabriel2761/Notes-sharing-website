72f7b6869418c7f7bc4b4abdb6ae0bff13caad93
gabriel2761 <niko.mojica@live.com>
2015-09-26 20:35:43 +1000

    create test for password matching in database

--- Login-confirm.php
+++ Login-confirm.php
@@ -1,7 +1,49 @@
+<?php include('/included-files/connect-database.php'); ?>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: nikom
- * Date: 26/09/2015
- * Time: 8:07 PM
- */
+$username = $_POST["username"];
+$password = $_POST["password"];
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Document</title>
+        <?php include('/included-files/head-setup.php'); ?>
+    </head>
+    <body>
+    <?php include('/included-files/navigation-bar.php'); ?>
+
+    <main class="login-confirm-main">
+
+
+        <?php
+
+        $query = "SELECT username, password FROM student
+                  WHERE username = '$username';";
+
+
+        $result = mysqli_query($connection, $query);
+        if (!$result) {
+            die("Database Query failed");
+        } else {
+
+            $row = mysqli_fetch_assoc($result);
+            if ($password == $row["password"]) {
+                echo 'passwords match!';
+
+            } else {
+                echo "passwords Don't match!";
+            }
+
+        }
+
+        ?>
+
+    </main>
+
+    <?php include('/included-files/scripts.php'); ?>
+    </body>
+    </html>
+
+<?php mysqli_close($connection) ?>
