d2fa959f53309875fbc03a1222a2ee59438eaf02
gabriel2761 <niko.mojica@live.com>
2015-09-26 16:37:11 +1000

    account creation enabled

--- confirmRegister.php
+++ confirmRegister.php
@@ -1,7 +1,43 @@
+<?php include('/included-files/connect-database.php'); ?>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: nikom
- * Date: 23/09/2015
- * Time: 10:28 PM
- */ 
+$first_name = $_POST["first_name"];
+$last_name = $_POST["last_name"];
+$username = $_POST["username"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Document</title>
+        <?php include('/included-files/head-setup.php'); ?>
+    </head>
+    <body>
+    <?php include('/included-files/navigation-bar.php'); ?>
+
+    <main class="register-confirm-main">
+
+
+        <?php
+
+        $query = "INSERT INTO student (first_name, last_name, username, password, email)
+                  VALUES ('$first_name','$last_name', '$username', '$password','$email');";
+
+        $result = mysqli_query($connection, $query);
+        if (!$result) {
+            die("Database Query failed");
+        } else {
+            echo "success!";
+        }
+
+        ?>
+
+    </main>
+
+    <?php include('/included-files/scripts.php'); ?>
+    </body>
+    </html>
+
+<?php mysqli_close($connection) ?>
