af88c087c32594e503cb7662e440c766b7ad1ac5
gabriel2761 <niko.mojica@live.com>
2015-10-02 06:37:23 +1000

    create input validation

--- src/jscript/register-form-check.js
+++ src/jscript/register-form-check.js
@@ -12,22 +12,54 @@ $(document).ready(function() {
     var email_status = $('#email-status');
     var password_input = $('#password-register');
     var password_status = $('#password-status');
-    var repass_input = $('#password_repeat');
+    var repass_input = $('#password-repeat');
     var repass_status = $('#repass-status');
 
     var input_check = 'input-checkers/check-register.php';
+    var required = 'Required';
+    var filled = 'tick.jpeg'
 
-    $(username_input).keyup(function() {
-        $.post(input_check, {username:username_input.val()},
-            function(result) {
-                username_status.text(result);
-            });
+    var passwordLength = "must be at least 6 characters";
+    var notMatching = "passwords don't match";
+
+    firstname_input.keyup(function() {
+        firstname_input.val().length < 1 ? firstname_status.text(required) : firstname_status.text(filled);
     });
 
-    $(email_input).keyup(function() {
-        $.post(input_check, {email:email_input.val()},
-            function(result) {
-                email_status.text(result);
-            });
+    lastname_input.keyup(function() {
+       lastname_input.val().length < 1 ? lastname_status.text(required) : lastname_status.text(filled);
     });
+
+    password_input.keyup(function() {
+        var pass = password_input.val();
+        pass.length < 6 ? password_status.text(passwordLength) : password_status.text(filled);
+        repass_input.val() == pass && pass.length != 0 ? repass_status.text(filled) : repass_status.text(notMatching);
+    });
+
+    repass_input.keyup(function() {
+        repass_input.val() === password_input.val() ? repass_status.text(filled) : repass_status.text(notMatching);
+    });
+
+    username_input.keyup(function() {
+        if (username_input.val().length < 1) {
+            username_status.text(required);
+        } else {
+            $.post(input_check, {username:username_input.val()},
+                function(result) {
+                    username_status.text(result);
+                });
+        }
+    });
+
+    email_input.keyup(function() {
+        if (email_input.val().length < 1) {
+            email_status.text(required);
+        } else {
+            $.post(input_check, {email:email_input.val()},
+                function(result) {
+                    email_status.text(result);
+                });
+            }
+       });
+
 });
