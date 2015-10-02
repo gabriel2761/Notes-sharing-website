577085be2b2dfe9756eb00e4eedd886a231f2688
gabriel2761 <niko.mojica@live.com>
2015-10-02 23:30:10 +1000

    change design of register confirm

--- confirm-register.php
+++ confirm-register.php
@@ -1,50 +1,51 @@
 <?php include('/included-files/connect-database.php'); ?>
 
 <?php
-$first_name = $_POST[POST_FIRST_NAME];
-$last_name = $_POST[POST_LAST_NAME];
-$username = $_POST[POST_USERNAME];
-$password = $_POST[POST_PASSWORD];
-$email = $_POST[POST_EMAIL];
+    $first_name = $_POST[POST_FIRST_NAME];
+    $last_name = $_POST[POST_LAST_NAME];
+    $username = $_POST[POST_USERNAME];
+    $password = $_POST[POST_PASSWORD];
+    $email = $_POST[POST_EMAIL];
 ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <title>Document</title>
         <?php include(HEADER_SETUP); ?>
     </head>
-    <body>
-    <?php include(NAVIGATION_BAR); ?>
+<body>
+<main id="register-confirm-main" class="text-center">
 
-    <main class="register-confirm-main">
+    <?php
+    $result = "";
 
+    // declare table column names
+    $student_table = STUDENT_TABLE_NAME;
+    $student_username = STUDENT_USERNAME;
+    $student_firstname = STUDENT_FIRST_NAME;
+    $student_lastname = STUDENT_LAST_NAME;
+    $student_password = STUDENT_PASSWORD;
+    $student_email = STUDENT_EMAIL;
 
-        <?php
-        // declare table column names
-        $student_table = STUDENT_TABLE_NAME;
-        $student_username = STUDENT_USERNAME;
-        $student_firstname = STUDENT_FIRST_NAME;
-        $student_lastname = STUDENT_LAST_NAME;
-        $student_password = STUDENT_PASSWORD;
-        $student_email = STUDENT_EMAIL;
+    $query  = "INSERT INTO $student_table ($student_username, $student_firstname,$student_lastname,$student_password, $student_email)";
+    $query .= "VALUES ('$username','$first_name','$last_name','$password','$email');";
 
-        $query  = "INSERT INTO $student_table ($student_username, $student_firstname,$student_lastname,$student_password, $student_email)";
-        $query .= "VALUES ('$username','$first_name','$last_name','$password','$email');";
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+        $result = "Something went wrong";
+    } else {
+        $result =  "Success!";
+    }
+    ?>
 
-        $result = mysqli_query($connection, $query);
-        if (!$result) {
-            die("Database Query failed");
-        } else {
-            echo "success!";
-        }
+    <h1><?php echo $result ?></h1>
 
-        ?>
-
-    </main>
+    <a href="index.php">back to main page</a>
 
     <?php include(SCRIPTS); ?>
-    </body>
-    </html>
+</main>1
+</body>
+</html>
 
 <?php mysqli_close($connection) ?>
