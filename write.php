2c8b8cddb1e3960571975d73170ac7908ad47972
erikko21 <erictran201@gmail.com>
2015-10-11 14:49:13 +1100

    add some front-end element in write.php and fix some styling display issue

--- write.php
+++ write.php
@@ -2,19 +2,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Document</title>
+    <title>New Note</title>
     <?php include(HEADER_SETUP); ?>
 </head>
 <body>
     <?php include(NAVIGATION_BAR); ?>
 
-    <main class="write-main">
+    <main id="write-main">
+	
+		<header>
+			<h1>New Note</h1>
+		</header>
+	
         <form method="post" action="review.php">
             <input name=<?php echo POST_NOTE_NUMBER; ?> type="text" placeholder="Subject No.">
             <input name=<?php echo POST_NOTE_TITLE; ?> type="text" placeholder="Title">
             <textarea placeholder="Write notes" name=<?php echo POST_NOTE_CONTENT; ?> id=""></textarea>
             <button type="submit" class="btn btn-danger">Submit</button>
         </form>
+		
+		<form action = "index.php">
+			<button type = "submit" class = "btn btn-danger">Cancel</button>
+		</form>
     </main>
 
     <?php include(SCRIPTS); ?>
