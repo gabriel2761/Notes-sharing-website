98de05f9d8caba61012b5b322ab7f4fbb48c91b2
erikko21 <erictran201@gmail.com>
2015-10-11 13:56:30 +1100

    optimise all pages for mobile

--- included-files/head-setup.php
+++ included-files/head-setup.php
@@ -1,4 +1,5 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="src/style/css/style.css">
